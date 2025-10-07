eed8fd61732a67b710a01e4dc39bf0476616f8a6
Halil Can Uyanık <101755107+halilcanuyanik@users.noreply.github.com>
2025-10-07 10:50:21 +0300

    set security http headers and limit payload size

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const userRouter = require('./routes/userRoutes');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-app.use(express.json());
+app.use(helmet());
+
+app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
 const limiter = rateLimit({
